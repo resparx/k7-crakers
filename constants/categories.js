2c858f2e6feedd89a682f92d36b357fc5ef925a3
resparx <ramprash.rspx@gmail.com>
2024-09-16 19:00:46 +0530

    feat: update categories

--- constants/categories.js
+++ constants/categories.js
@@ -1,14 +1,85 @@
 export const categories = [
     {
-        name: 'Onse sound crackers',
+        name: "ONE SOUND CRACKERS",
         id: 1
     },
     {
-        name: 'Mini Lar',
+        name: "MINI LAR",
         id: 2
     },
     {
-        name: "",
+        name: "DELUXE CRACKERS",
         id: 3
-    }
-]
+    },
+ {
+        name: "GARLAND CRACKERS",
+        id: 4
+    },
+
+ {
+        name: "DUM BIJILI",
+        id: 5
+    },
+ {
+        name: "ATOM BOM",
+        id: 6
+    },
+
+   {
+        name: "BABY CANDLE",
+        id: 7
+    },
+   {
+        name: "ROCKETS",
+        id: 8
+    },
+   {
+        name: "GROUND CHAKKARA",
+        id: 9
+    },
+{
+        name: "SPL WHEELS",
+        id: 10
+    },
+{
+        name: "FLOWER POTS",
+        id: 11
+    },
+
+{
+        name: "KIDS ENJOYMENT",
+        id: 12
+    },
+{
+        name: "FUN NIGHT",
+        id: 13
+    },
+{
+        name: "BEAUTY FOUNTAIN",
+        id: 14
+    },
+{
+        name: "TWINLING STAR",
+        id: 15
+    },
+{
+        name: "SPARCKLERS",
+        id: 16
+    },
+{
+        name: "CHILDENS ITEMS",
+        id: 17
+    },
+{
+        name: "FANCY SHOTS",
+        id: 18
+    },
+{
+        name: "HIGH LEVEL SHOTS",
+        id: 19
+    },
+{
+        name: "GIFT BOXES ( NO DISCOUNT)",
+        id: 20
+    },
+];
